3e698ef59dba70ce73b4735bcf9f983dce5608c1
Pauljesmarc <151122008+Pauljesmarc@users.noreply.github.com>
2023-12-06 13:37:34 +0800

    Update Main.java

--- FINALPROJECT/src/Main.java
+++ FINALPROJECT/src/Main.java
@@ -1,17 +1,28 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import javax.swing.*;
+import java.util.*;
+
+public class Main extends JFrame {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        game app = new game();
+        app.setTitle("UGANG LEGENDS");
+        app.setSize(1000,500);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+//        Director director = new Director();
+//        EntityBuilder entityBuilder = new EntityBuilder();
+//
+//        director.buildPlayer1(entityBuilder);
+//        Entity p1 = entityBuilder.build();
+//
+//        director.buildBoss(entityBuilder);
+//        Entity boss = entityBuilder.build();
+//
+//
+//        System.out.println(p1.toString() + '\n');
+//        System.out.println(boss.toString() + '\n');
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+
+
     }
 }
