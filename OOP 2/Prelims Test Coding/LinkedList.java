1605db62a3855dab870946375e7d9afcd7fff0eb
Fleurette <135421389+Anato-Eini@users.noreply.github.com>
2024-02-20 05:55:53 +0800

    Add files via upload

---
+++ OOP 2/Prelims Test Coding/LinkedList.java
@@ -0,0 +1,51 @@
+package Codes;
+
+public class LinkedList<T> {
+    class Node {
+        public T element;
+        public Node next;
+        Node(T element, Node next){
+            this.element = element;
+            this.next = next;
+        }
+    }
+    private Node head, tail;
+    int size;
+    public LinkedList(){
+        head = null;
+        tail = null;
+        size = 0;
+    }
+    public void add(T element){
+        if(head == null){
+            tail = head = new Node(element, null);
+        }else{
+            tail = tail.next = new Node(element, null);
+        }
+        size++;
+    }
+
+    public T set(T element, int pos) {
+        if(pos < 1 || pos > size)
+            throw new IllegalArgumentException("Illegal Position");
+        Node curr = head;
+        int i = 1;
+        while(i < pos - 1){
+            curr = curr.next;
+            i++;
+        }
+        T out;
+        out = curr.element;
+        curr.element = element;
+        return out;
+    }
+
+    public void print(){
+        Node curr = head;
+        while(curr != null) {
+            System.out.print(curr.element + " ");
+            curr = curr.next;
+        }
+    }
+
+}
