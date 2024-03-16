1578b9f0a5a135d7c6e56b4c71daaaf9bf0ee11b
Anato-Eini <jameskennethacabal@gmail.com>
2024-03-16 11:19:00 +0800

    Fix login

--- connect.php
+++ connect.php
@@ -4,6 +4,8 @@ $connection = new mysqli('localhost', 'root', '', 'dbacabalf3');
 if(!$connection->connect_error){
     error_log (mysqli_error($connection));
 }
+
+$message = '';
 if(isset($_POST['btnRegister'])){
     //retrieve data from form and save the value to a variable
     //for tbluserprofile
@@ -23,10 +25,9 @@ if(isset($_POST['btnRegister'])){
     $numLastName = mysqli_num_rows(mysqli_query($connection,
         "Select * from tbluserprofile where lastname='".$lname."'"));
     $numPassword = mysqli_num_rows(mysqli_query($connection,
-        "Select * from tbluseraccount where password='".$pword."'"));
+        "Select * from tbluseraccount where password='".password_hash($pword, PASSWORD_DEFAULT)."'"));
     $numEmailAdd = mysqli_num_rows(mysqli_query($connection,
         "Select * from tbluseraccount where emailadd='".$email."'"));
-    $message = '';
     if($numUsername > 0)
         $message = "User name already exists";
     else if($numFirstName > 0)
@@ -38,11 +39,13 @@ if(isset($_POST['btnRegister'])){
     else if($numEmailAdd > 0)
         $message = "Email Address already exists";
     else{
-        $sql1 ="Insert into tbluserprofile(firstname,lastname,gender) values('".$fname."','".$lname."','".$gender."')";
+        $sql1 ="Insert into tbluserprofile(firstname,lastname,gender) 
+                values('".$fname."','".$lname."','".$gender."')";
         mysqli_query($connection,$sql1);
-        $sql ="Insert into tbluseraccount(emailadd,username,password, usertype) values('".$email."','".$uname."','".$pword."', 'employee')";
+        $sql ="Insert into tbluseraccount(emailadd,username,password, usertype) 
+                values('".$email."','".$uname."','".password_hash($pword, PASSWORD_DEFAULT)."', 'employee')";
         mysqli_query($connection,$sql);
-        $message = "Registered successfully";
+        $message = "Registered Successfully";
     }
     echo "<script>
                        $('#modalMessage').get(0).textContent = '".$message."';
@@ -56,10 +59,24 @@ if(isset($_POST['btnRegister'])){
 if(isset($_GET['btnLogin'])){
     $username = $_GET['txtUsername'];
     $password = $_GET['txtPassword'];
-    $sql_query = mysqli_query($connection, "Select * from tbluseraccount where username='".$username."'");
-    if(mysqli_num_rows($sql_query) > 0)
-        echo "<script>window.location.href = 'mainPage.php'</script>";
-    else{
+
+    $sqlQuery = mysqli_query($connection,
+        "Select password from tbluseraccount where username='".$username."'");
+    if($sqlQuery) {
+        $row = mysqli_fetch_assoc($sqlQuery);
+
+        if(password_verify($password, $row['password'])) {
+            echo "<script>
+            window.location.href = 'mainPage.php'</script>";
+            exit();
+        }else{
+            echo "<script>
+            $('.modalMessage').get(0).textContent = 'Wrong Password database ".$row['password']."';
+            const myModalAlternative = new bootstrap.Modal('#modalAlreadyExists', null);
+            myModalAlternative.show(myModalAlternative);
+            </script>";
+        }
+    }else{
         echo "<script>
             const myModalAlternative = new bootstrap.Modal('#modalAlreadyExists', null);
             myModalAlternative.show(myModalAlternative);
