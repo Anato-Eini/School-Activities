88a975e568f61f4b47c3ac0b62b6e796203aa5c2
Anato-Eini <jameskennethacabal@gmail.com>
2024-03-19 00:09:01 +0800

    Changing Directories && Perfect Number Partial

---
+++ src/main/java/com/example/javafx1/PerfectNumber/PerfectNumberMainPage.java
@@ -0,0 +1,16 @@
+package com.example.javafx1.PerfectNumber;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class PerfectNumberMainPage {
+    public VBox numLTPN;
+    public VBox numPN;
+    public VBox numMTPN;
+    @FXML
+    HBox progressBars;
+    public void startProcess(){
+
+    }
+}
