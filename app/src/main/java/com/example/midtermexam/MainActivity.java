8cffb03f9b522d66d6855a273b0e2fab61bbe6cb
Destiny <jameskennethacabal@gmail.com>
2024-03-18 20:02:06 +0800

    Completion

--- app/src/main/java/com/example/midtermexam/MainActivity.java
+++ app/src/main/java/com/example/midtermexam/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.midtermexam;
 
+import static android.graphics.Color.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -17,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(
+                getApplicationContext(),
+                "James Kenneth S. Acabal - TicTacToe", Toast.LENGTH_LONG).show();
         Button[] buttons = {
                 findViewById(R.id.button1),
                 findViewById(R.id.button2),
@@ -38,11 +44,9 @@ public class MainActivity extends AppCompatActivity {
                 if(!isClicked.get(index).get()){
                     isClicked.get(index).set(true);
                     buttons[index].setText(isPlayer1Turn.get() ? "O" : "X");
-                    background.setBackgroundColor(isPlayer1Turn.get() ?
-                            getResources().getColor(R.color.red):
-                            getResources().getColor(R.color.blue));
+                    background.setBackgroundColor(isPlayer1Turn.get() ? RED: BLUE);
                     isPlayer1Turn.set(!isPlayer1Turn.get());
-                    display.setText(isPlayer1Turn.get() ? "Player O\'s Turn": "Player X\'s Turn");
+                    display.setText(isPlayer1Turn.get() ? "Player O's Turn" : "Player X's Turn");
                 }
             });
         }
