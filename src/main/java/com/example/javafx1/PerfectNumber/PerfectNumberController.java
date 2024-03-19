6323393b6e10789d98259016a799b5693d05eefd
Destiny <jameskennethacabal@gmail.com>
2024-03-19 17:15:29 +0800

    Cleanup Perfect Numbers

--- src/main/java/com/example/javafx1/PerfectNumber/PerfectNumberController.java
+++ src/main/java/com/example/javafx1/PerfectNumber/PerfectNumberController.java
@@ -7,7 +7,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.util.regex.*;
