bf6798c2652c0a14c89bac26512c18b10aba0f9c
Destiny <jameskennethacabal@gmail.com>
2024-03-19 20:45:13 +0800

    Reducing lines

--- src/main/java/com/example/javafx1/FibonacciSequence/FibonacciSequence.java
+++ src/main/java/com/example/javafx1/FibonacciSequence/FibonacciSequence.java
@@ -1,22 +1,21 @@
 package com.example.javafx1.FibonacciSequence;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FibonacciSequence {
-    static ArrayList<Thread> threads;
+    static Thread[] threads;
     static int[] results;
     public static void main(String[] args) throws InterruptedException {
         Scanner sc = new Scanner(System.in);
         System.out.print("Until what number? ");
         int n = sc.nextInt();
-        threads = new ArrayList<>(n);
+        threads = new Thread[n];
         results = new int[n];
         for(int i = 0; i < n; i++)
-            threads.add(i, new Thread(new FibRunnable(i)));
+            threads[i] = new Thread(new FibRunnable(i));
 
-        threads.getLast().start();
-        threads.getLast().join();
+        threads[n - 1].start();
+        threads[n - 1].join();
         System.out.println("Fibonacci Sequence: ");
         for(int i: results)
             System.out.print(i + " ");
