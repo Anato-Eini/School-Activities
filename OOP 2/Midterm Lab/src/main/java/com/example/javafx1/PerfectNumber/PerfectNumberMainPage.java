32f443359cbdcb6ef01920f2f94049b16cf0a22c
Fleurette <135421389+Anato-Eini@users.noreply.github.com>
2024-03-23 18:52:22 +0800

    Partial Implementation of Threads

--- OOP 2/Midterm Lab/src/main/java/com/example/javafx1/PerfectNumber/PerfectNumberMainPage.java
+++ OOP 2/Midterm Lab/src/main/java/com/example/javafx1/PerfectNumber/PerfectNumberMainPage.java
@@ -1,6 +1,8 @@
 package com.example.javafx1.PerfectNumber;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -8,9 +10,15 @@ public class PerfectNumberMainPage {
     public VBox numLTPN;
     public VBox numPN;
     public VBox numMTPN;
+    public Button returnBtn;
     @FXML
-    HBox progressBars;
+    public HBox progressBars;
     public void startProcess(){
-
+        Thread[] threads = new Thread[PerfectNumberController.numThreads];
+        for(int i = 0; i < PerfectNumberController.numThreads; i++){
+            ProgressBar progressBar = new ProgressBar();
+            progressBar.setProgress(0);
+            progressBars.getChildren().add(progressBar);
+        }
     }
 }
