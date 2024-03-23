e01adde54a4ab4cf2ea02439fed2e2164c620ad8
Fleurette <135421389+Anato-Eini@users.noreply.github.com>
2024-03-23 18:53:02 +0800

    Improving BFSNodeSearch for handling ScrollPanes

--- OOP 2/Midterm Lab/src/main/java/com/example/javafx1/BFSNodeSearch.java
+++ OOP 2/Midterm Lab/src/main/java/com/example/javafx1/BFSNodeSearch.java
@@ -2,6 +2,7 @@ package com.example.javafx1;
 
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.ScrollPane;
 
 import java.util.LinkedList;
 
@@ -14,9 +15,13 @@ public class BFSNodeSearch extends Parent{
             for(Node node : currentNode.getChildrenUnmodifiable()){
                 if(node.getId() != null && node.getId().equals(name))
                     return (Parent) node;
+                else if(node instanceof ScrollPane)
+                    array.addLast((Parent) ((ScrollPane) node).getContent());
                 else if(node instanceof Parent)
                     array.addLast((Parent) node);
             }
+            if(currentNode.getId() != null && currentNode.getId().equals(name))
+                return currentNode;
         }
         return null;
     }
