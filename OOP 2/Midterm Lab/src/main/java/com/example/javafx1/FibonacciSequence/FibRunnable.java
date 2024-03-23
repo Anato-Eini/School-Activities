ffafd971c03fd013dfedc5c294df881b37c52f36
Fleurette <135421389+Anato-Eini@users.noreply.github.com>
2024-03-23 19:00:18 +0800

    Reducing lines

--- OOP 2/Midterm Lab/src/main/java/com/example/javafx1/FibonacciSequence/FibRunnable.java
+++ OOP 2/Midterm Lab/src/main/java/com/example/javafx1/FibonacciSequence/FibRunnable.java
@@ -9,24 +9,17 @@ public class FibRunnable implements Runnable{
     public void run() {
         if(id == 0){
             FibonacciSequence.results[id] = 1;
-            synchronized (FibonacciSequence.threads.get(id)){
-                FibonacciSequence.threads.get(id).notifyAll();
-            }
             return;
         }
-        synchronized (FibonacciSequence.threads.get(id - 1)){
+        synchronized (FibonacciSequence.threads[id - 1]){
             try {
-                FibonacciSequence.threads.get(id - 1).start();
-                FibonacciSequence.threads.get(id - 1).wait();
+                FibonacciSequence.threads[id - 1].start();
+                FibonacciSequence.threads[id - 1].wait();
             } catch (InterruptedException ignored) {}
         }
         if(id != 1)
             FibonacciSequence.results[id] = FibonacciSequence.results[id - 1] + FibonacciSequence.results[id - 2];
         else
             FibonacciSequence.results[id] = 2;
-
-        synchronized (FibonacciSequence.threads.get(id)){
-            FibonacciSequence.threads.get(id).notifyAll();
-        }
     }
 }
