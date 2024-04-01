adefa10d46b015ce290ad7e66a10662a2a39789b
Destiny <jameskennethacabal@gmail.com>
2024-04-02 00:46:52 +0800

    Connect 3

--- app/src/main/java/com/example/androidprojectcollection/Connect3.java
+++ app/src/main/java/com/example/androidprojectcollection/Connect3.java
@@ -7,9 +7,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class Connect3 extends AppCompatActivity {
     AtomicBoolean isBlack = new AtomicBoolean(true),
@@ -20,6 +18,7 @@ public class Connect3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect3);
         TextView displayText = findViewById(R.id.DisplayText);
+        Button resetBtn = findViewById(R.id.resetBtn);
         Button[][] slots = {
                 {
                     findViewById(R.id.button18),
@@ -64,30 +63,51 @@ public class Connect3 extends AppCompatActivity {
             slots[0][i].setOnClickListener(view -> {
                 if(slotsLeft[dupI] > -1 && !gameEnd.get()){
                     fillSlot(dupI, slots, slotsLeft, displayText);
+                    if(!gameEnd.get())
+                        displayText.setText(isBlack.get() ? getString(R.string.black_turn) :
+                                getString(R.string.red_turn));
                 }
             });
         }
+
+        resetBtn.setOnClickListener(view -> {
+            for(int i = 0; i < 5; i++)
+                slotsLeft[i] = 4;
+            displayText.setText(getString(R.string.black_turn));
+            for(Button[] B: slots){
+                for(Button b: B)
+                    b.setBackgroundColor(Color.WHITE);
+            }
+            gameEnd.set(false);
+            isBlack.set(true);
+        });
+
     }
     public void fillSlot(int column, Button[][] slots, int[] slotsLeft, TextView displayText){
         slots[slotsLeft[column]][column].setBackgroundColor(isBlack.get() ? Color.BLACK : Color.RED);
         checker(slots, slotsLeft[column], column, displayText);
-        slotsLeft[column]--;
         isBlack.set(!isBlack.get());
+        slotsLeft[column]--;
     }
 
     public void checker(Button[][] slots, int row, int column, TextView displayText){
-        int[] dx = {0, 1, 1, 1, 0, -1, -1, -1};
-        int[] dy = {-1, -1, 0, 1, 1, 1, 0, -1};
-        int result = 0;
-        for(int i = 0; i < dx.length; i++){
+        int[] dx = {0, 1, 1, 1, 0, -1, -1, -1},
+                dy = {-1, -1, 0, 1, 1, 1, 0, -1};
+
+        for(int i = 0; i < 8; i++){
             if(column + dx[i] < 5 && column + dx[i] >= 0 && row + dy[i] >= 0 && row + dy[i] < 5 &&
-            ((ColorDrawable)slots[column][row].getBackground()).getColor() ==
-                    ((ColorDrawable) slots[column + dx[i]][row + dy[i]].getBackground()).getColor())
-                result += checkerHelper(slots, dx[i], dy[i], row, column, slots[row][column]);
-        }
-        if(result >= 3){
-            gameEnd.set(true);
-            displayText.setText((isBlack.get() ? "BLACK" : "RED") .concat(" wins"));
+            ((ColorDrawable)slots[row][column].getBackground()).getColor() ==
+                    ((ColorDrawable) slots[row + dy[i]][column + dx[i]].getBackground()).getColor()) {
+                int result = 1;
+                result += checkerHelper(slots, dy[i], dx[i], row,
+                        column, slots[row][column]);
+                result += checkerHelper(slots, -dy[i], -dx[i], row, column, slots[row][column]);
+                if(result >= 3){
+                    gameEnd.set(true);
+                    displayText.setText(isBlack.get() ? getString(R.string.black_win) :
+                            getString(R.string.red_win));
+                }
+            }
         }
     }
 
@@ -95,12 +115,11 @@ public class Connect3 extends AppCompatActivity {
                              int row, int column, Button slotRef){
         row += directionRow;
         column += directionColumn;
-        if(row < 0 || column < 0 || row >= 5 || column >= 5)
+        if(row < 0 || column < 0 || row >= 5 || column >= 5 ||
+                (((ColorDrawable)slots[row][column].getBackground()).getColor() !=
+                ((ColorDrawable) slotRef.getBackground()).getColor()))
             return 0;
-        else if(((ColorDrawable)slots[row][column].getBackground()).getColor() ==
-                ((ColorDrawable) slotRef.getBackground()).getColor())
+        else
             return checkerHelper(slots, directionRow, directionColumn, row, column, slotRef) + 1;
-
-        return 0;
     }
 }
