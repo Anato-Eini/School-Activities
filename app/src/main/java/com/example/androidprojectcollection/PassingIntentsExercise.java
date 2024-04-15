ad08d13b2e26c3615767efc61372bb2a10cf1dca
L11X07W35 <jameskennethacabal@gmail.com>
2024-04-15 10:32:26 +0800

    Partial

---
+++ app/src/main/java/com/example/androidprojectcollection/PassingIntentsExercise.java
@@ -0,0 +1,14 @@
+package com.example.androidprojectcollection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioButton;
+
+public class PassingIntentsExercise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passing_intents_exercise);
+    }
+}
---
+++ app/src/main/java/com/example/androidprojectcollection/PassingIntentsExercise2.java
@@ -0,0 +1,13 @@
+package com.example.androidprojectcollection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PassingIntentsExercise2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passing_intents_exercise2);
+    }
+}
