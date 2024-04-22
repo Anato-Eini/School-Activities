638d7fb43c7bb322e61cad06f7e866daa045ae2e
Destiny <jameskennethacabal@gmail.com>
2024-04-22 18:39:20 +0800

    Changes: -Improved performance when submitting a reply, or deleting a post -Update Post

--- src/main/java/com/example/forum/MainPageController.java
+++ src/main/java/com/example/forum/MainPageController.java
@@ -2,13 +2,12 @@ package com.example.forum;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
@@ -17,6 +16,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class MainPageController {
     public AnchorPane mainPageContainer;
@@ -33,11 +33,6 @@ public class MainPageController {
             while (postRecords.next()) {
                 HBox header = new HBox();
                 Label name = new Label(getUser(postRecords.getInt("author_id")));
-                header.getChildren().add(name);
-                if(LogInController.idSession == postRecords.getInt("author_id")){
-
-                    header.getChildren().add(createDeletePostBtn(postRecords.getInt("id")));
-                }
                 name.getStyleClass().add("names");
                 name.setWrapText(true);
                 Text postContent = new Text(postRecords.getString("body"));
@@ -58,26 +53,28 @@ public class MainPageController {
                     replyUsername.getStyleClass().add("replyUsernames");
                     Text replyContent = new Text(replySets.getString("body"));
                     replyContent.getStyleClass().add("replyContents");
-                    replyContainer.getChildren().add(replyUsername);
-                    replyContainer.getChildren().add(replyContent);
+                    replyContainer.getChildren().addAll(replyUsername, replyContent);
+                    //TODO implement delete own replies
+                    //TODO implement edit own replies
 
                     replies.getChildren().add(replyContainer);
                 }
 
                 HBox newReply = new HBox();
                 TextField theReply = new TextField();
-                Button submitReply = createSubmitButton(
-                        theReply, LogInController.idSession, postRecords.getInt("id"));
-                newReply.getChildren().add(theReply);
-                newReply.getChildren().add(submitReply);
+                newReply.getChildren().addAll(theReply, createSubmitButton(
+                        theReply, LogInController.idSession, postRecords.getInt("id"), replies));
                 replies.getChildren().add(newReply);
-                //TODO implement delete own replies
-                //TODO implement edit own replies
 
                 post.getStyleClass().add("post");
-                post.getChildren().add(header);
-                post.getChildren().add(postContent);
-                post.getChildren().add(replies);
+                post.getChildren().addAll(header, postContent, replies);
+                if(LogInController.idSession == postRecords.getInt("author_id"))
+                    header.getChildren().addAll(
+                            name,
+                            createEditPostBtn(postRecords.getInt("id"), postContent, post),
+                            createDeletePostBtn(postRecords.getInt("id"), post));
+                header.getChildren().forEach(node ->
+                        HBox.setMargin(node, new Insets(0, 10, 0, 0)));
                 posts.getChildren().addFirst(post);
             }
         }catch (SQLException e){
@@ -85,16 +82,60 @@ public class MainPageController {
         }
     }
 
-    private Button createEditPostBtn(int post_id){
-        Button editPostBtn = new Button();
+    private Button createEditPostBtn(int post_id, Text postContent, VBox post){
+        Button editPostBtn = new Button(), cancelEditPostBtn = new Button(), submitEditPostBtn = new Button();
         editPostBtn.setText("Edit Post");
+        cancelEditPostBtn.setText("Cancel");
+        submitEditPostBtn.setText("Submit");
+
         editPostBtn.setOnMouseClicked(_ -> {
-            //TODO implement edit own post
+            AtomicReference<String> bodyPost = new AtomicReference<>(postContent.getText());
+            HBox header = (HBox)post.getChildren().getFirst();
+            TextArea editBody = new TextArea(bodyPost.get());
+            Button editButton = (Button) header.getChildren().get(1);
+            header.getChildren().remove(editButton);
+            post.getChildren().remove(1);
+            VBox editPostContainer = new VBox();
+            HBox optionButtons = new HBox();
+            editBody.setPrefWidth(Region.USE_COMPUTED_SIZE);
+            editBody.setWrapText(true);
+            editBody.setPrefHeight(50);
+            optionButtons.getChildren().addAll(cancelEditPostBtn, submitEditPostBtn);
+            editPostContainer.getChildren().addAll(editBody, optionButtons);
+            post.getChildren().add(1, editPostContainer);
+
+            cancelEditPostBtn.setOnMouseClicked(_ -> {
+                editBody.setText(bodyPost.get());
+                post.getChildren().remove(editPostContainer);
+                post.getChildren().add(1, postContent);
+                header.getChildren().add(1, editButton);
+            });
+
+            submitEditPostBtn.setOnMouseClicked(_ -> {
+                if(editBody.getText().isEmpty())
+                    status.setText("Can't leave as blank");
+                else if(!editBody.getText().equals(bodyPost.get())){
+                    try{
+                        ForumApplication.connection.createStatement().execute(
+                                STR."UPDATE tblpost SET body='\{editBody.getText()}' WHERE id=\{post_id}"
+                        );
+                        postContent.setText((editBody.getText()));
+                        post.getChildren().add(1, postContent);
+                        header.getChildren().add(1, editButton);
+                        post.getChildren().remove(editPostContainer);
+                        bodyPost.set(editBody.getText());
+                        status.setText("Edit successfully");
+                    }catch (SQLException e){
+                        e.printStackTrace();
+                    }
+                }else
+                    status.setText("Same Content");
+            });
         });
         return editPostBtn;
     }
 
-    private Button createDeletePostBtn(int post_id) {
+    private Button createDeletePostBtn(int post_id, VBox post) {
         Button deletePostBtn = new Button();
         deletePostBtn.setText("Delete Post");
         deletePostBtn.setOnMouseClicked(_ -> {
@@ -103,8 +144,8 @@ public class MainPageController {
                 statement.addBatch(STR."DELETE FROM tblpost WHERE id = \{post_id}");
                 statement.addBatch(STR."DELETE FROM tblreply WHERE post_id = \{post_id}");
                 statement.executeBatch();
-                posts.getChildren().clear();
-                getPosts();
+                posts.getChildren().remove(post);
+                status.setText("Delete post successfully");
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -112,16 +153,31 @@ public class MainPageController {
         return deletePostBtn;
     }
 
-    private Button createSubmitButton(TextField theReply, int author_id, int post_id) {
+    private Button createSubmitButton(TextField theReply, int author_id, int post_id, VBox replies) {
         Button submitReply = new Button();
         submitReply.setText("Submit Reply");
         submitReply.setOnMouseClicked(_ -> {
             if(theReply.getText().isEmpty())
                 status.setText("Empty Reply");
             else{
-                reply(post_id, author_id, theReply.getText());
-                posts.getChildren().clear();
-                getPosts();
+                try {
+                    ResultSet results = ForumApplication.connection.createStatement().executeQuery(
+                            STR."SELECT name FROM tbluseracct WHERE id = \{author_id}");
+                    results.next();
+                    String authorUsername = results.getString("name");
+                    VBox replyContainer = new VBox();
+                    replyContainer.getStyleClass().add("replyContainers");
+                    Label replyUsername = new Label(authorUsername);
+                    replyUsername.getStyleClass().add("replyUsernames");
+                    Text replyContent = new Text(theReply.getText());
+                    replyContent.getStyleClass().add("replyContents");
+                    replyContainer.getChildren().addAll(replyUsername, replyContent);
+                    replies.getChildren().add(replies.getChildren().size() - 1, replyContainer);
+                    reply(post_id, author_id, theReply.getText());
+                    theReply.setText("");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
         });
         return submitReply;
