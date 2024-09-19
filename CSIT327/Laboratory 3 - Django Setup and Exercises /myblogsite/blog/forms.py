e401c6ad4751e155cde5cdcff9bed6c76a8a7413
Fleurette <secret@linux.com>
2024-09-19 14:29:12 +0800

    Login and Registration

--- CSIT327/Laboratory 3 - Django Setup and Exercises /myblogsite/blog/forms.py
+++ CSIT327/Laboratory 3 - Django Setup and Exercises /myblogsite/blog/forms.py
@@ -2,18 +2,53 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control'}), label="Username"
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control'
+            }
+        ),
+        label="Username"
     )
+
     password = forms.CharField(
-        widget=forms.PasswordInput, label="Password"
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control'
+            }
+        ),
+        label="Password"
     )
 
 class RegisterForm(forms.Form):
     username = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control'}), label='Enter your username',max_length=100
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control'
+            }
+        ),
+        label='Enter your username',
+        max_length=100
     )
+
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-control'}), label='Password'
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control'
+            }
+        ),
+        label='Password'
     )
-    email = forms.EmailField(label='Enter your email')
-    birth_date = forms.DateField(label='Enter your birth date')
+
+    email = forms.EmailField(
+        label='Enter your email'
+    )
+
+    birth_date = forms.DateField(
+        widget=forms.DateInput(
+            attrs={
+                'type': 'date',
+                'class': 'form-control',
+            }
+        ),
+        label='Enter your birth date'
+    )
--- CSIT327/Laboratory 3 - Django Setup and Exercises /myblogsite/blog/views.py
+++ CSIT327/Laboratory 3 - Django Setup and Exercises /myblogsite/blog/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.password_validation import validate_password
 
-from .models import Post, Comment
+from .models import Post, Comment, User
 from .forms import *
 
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'post_list.html', {'posts': posts})
+    return render(request, 'post_list.html', {'posts': Post.objects.all()})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -16,21 +17,49 @@ def render_login(request):
     return render(request, 'login_form.html', {'form' : LoginForm()})
 
 def render_register(request):
-    return render(request, 'register_form.html', {'form' : RegisterForm()})
+    message = ''
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            birth_date = form.cleaned_data.get('birth_date')
+
+            if User.objects.filter(username=username).exists():
+                message = 'Username already taken'
+            else:
+               User(
+                    username = username,
+                    password = make_password(password),
+                    email = email,
+                    birth_date = birth_date
+               ).save()
+               return render(request, 'login_form.html')
+        else:
+            message = 'Invalid Form'
+
+    return render(request, 'register_form.html', {'message': message, 'form' : RegisterForm()})
 
 def login_post(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data.get('name')
+            username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-            birth_date = form.cleaned_data.get('birth_date')
-            email = form.cleaned_data.get('email')
-            return render(request, '/success.html', {'name' : name})
+
+            try:
+                user = User.objects.get(username=username)
+                if user and user.check_password(password):
+                    request.session['username'] = username
+                    return redirect('home')
+            except:
+                form.add_error('username', 'Username does not exist')
         else:
+            message = 'Invalid Form'
             form = LoginForm()
 
-        return render(request, 'login.html', )
+    return render(request, 'login.html', context={'form': form, 'message': message})
 
 def post_new(request):
     pass
