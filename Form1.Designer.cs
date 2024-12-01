0bfac8695704b23064de2be23e6c2627c229b823
Fleurette <135421389+Anato-Eini@users.noreply.github.com>
2024-12-01 23:40:25 +0800

    Working Model

---
+++ Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace Backpropagation_Neural_Networks
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label1 = new Label();
+            label3 = new Label();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            label4 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label2 = new Label();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            label5 = new Label();
+            button5 = new Button();
+            textBox9 = new TextBox();
+            label6 = new Label();
+            textBox10 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(64, 120);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(64, 149);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(64, 178);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(64, 207);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(98, 93);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Inputs";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(332, 149);
+            label3.Name = "label3";
+            label3.Size = new Size(60, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Real Value";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(311, 167);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(477, 167);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(489, 149);
+            label4.Name = "label4";
+            label4.Size = new Size(75, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Rounded Off";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(673, 74);
+            button1.Name = "button1";
+            button1.Size = new Size(115, 52);
+            button1.TabIndex = 10;
+            button1.Text = "Initialize Neural Network";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(673, 132);
+            button2.Name = "button2";
+            button2.Size = new Size(66, 52);
+            button2.TabIndex = 11;
+            button2.Text = "Train";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(673, 190);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 52);
+            button3.TabIndex = 12;
+            button3.Text = "Test";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(80, 368);
+            button4.Name = "button4";
+            button4.Size = new Size(100, 70);
+            button4.TabIndex = 13;
+            button4.Text = "Find Minimum Hidden Neurons";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(80, 337);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 15);
+            label2.TabIndex = 14;
+            label2.Text = "Minimum Neurons";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(203, 334);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(100, 23);
+            textBox7.TabIndex = 15;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(484, 334);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(100, 23);
+            textBox8.TabIndex = 18;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(361, 337);
+            label5.Name = "label5";
+            label5.Size = new Size(101, 15);
+            label5.TabIndex = 17;
+            label5.Text = "Minimum Epochs";
+            // 
+            // button5
+            // 
+            button5.Location = new Point(361, 368);
+            button5.Name = "button5";
+            button5.Size = new Size(100, 70);
+            button5.TabIndex = 16;
+            button5.Text = "Find Minimum Epochs";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(435, 40);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(100, 23);
+            textBox9.TabIndex = 20;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(420, 22);
+            label6.Name = "label6";
+            label6.Size = new Size(140, 15);
+            label6.TabIndex = 19;
+            label6.Text = "Current HNeurons Count";
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(271, 40);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(100, 23);
+            textBox10.TabIndex = 22;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(271, 22);
+            label7.Name = "label7";
+            label7.Size = new Size(83, 15);
+            label7.TabIndex = 21;
+            label7.Text = "Current Epoch";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            label8.ForeColor = Color.IndianRed;
+            label8.Location = new Point(64, 43);
+            label8.Name = "label8";
+            label8.Size = new Size(0, 20);
+            label8.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label8);
+            Controls.Add(textBox10);
+            Controls.Add(label7);
+            Controls.Add(textBox9);
+            Controls.Add(label6);
+            Controls.Add(textBox8);
+            Controls.Add(label5);
+            Controls.Add(button5);
+            Controls.Add(textBox7);
+            Controls.Add(label2);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label1;
+        private Label label3;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label label4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label2;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private Label label5;
+        private Button button5;
+        private TextBox textBox9;
+        private Label label6;
+        private TextBox textBox10;
+        private Label label7;
+        private Label label8;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+using Backprop;
+
+namespace Backpropagation_Neural_Networks
+{
+    public partial class Form1 : Form
+    {
+        NeuralNet? neuralNet;
+
+        int currentEpoch;
+
+        int[,] dataset;
+
+        /// <summary>
+        /// Initializes form
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            dataset = new int[16, 5]
+            {
+                {0, 0, 0, 0, 0 },
+                {0, 0, 0, 1, 0 },
+                {0, 0, 1, 0, 0 },
+                {0, 0, 1, 1, 0 },
+                {0, 1, 0, 0, 0 },
+                {0, 1, 1, 1, 0 },
+                {0, 1, 1, 0, 0 },
+                {0, 1, 0, 1, 0 },
+                {1, 0, 1, 0, 0 },
+                {1, 0, 1, 1, 0 },
+                {1, 0, 0, 0, 0 },
+                {1, 0, 1, 1, 0 },
+                {1, 1, 1, 0, 0 },
+                {1, 1, 0, 1, 0 },
+                {1, 1, 1, 0, 0 },
+                {1, 1, 1, 1, 1 }
+            };
+        }
+
+        /// <summary>
+        /// Initialize Neural Network
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label8.Text = "";
+            currentEpoch = 0;
+            neuralNet = new NeuralNet(4, 20, 1);
+            textBox9.Text = 20.ToString();
+            textBox10.Text = currentEpoch.ToString();
+        }
+
+        /// <summary>
+        /// The network is considered good if it passed all the tests
+        /// </summary>
+        /// <returns></returns>
+        private bool fitTest()
+        {
+            if (neuralNet == null)
+                return false;
+
+            for (int i = 0; i < 16; i++)
+            {
+                neuralNet.setInputs(0, dataset[i, 0]);
+                neuralNet.setInputs(1, dataset[i, 1]);
+                neuralNet.setInputs(2, dataset[i, 2]);
+                neuralNet.setInputs(3, dataset[i, 3]);
+                neuralNet.run();
+
+                if (Math.Round(neuralNet.getOuputData(0)) != dataset[i, 4])
+                    return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Neural Network learns from the dataset
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (neuralNet == null)
+            {
+                label8.Text = "Neural Network not initialized!";
+                return;
+            }
+
+            label8.Text = "";
+
+            while (!fitTest())
+            {
+                for (int i = 0; i < 16; i++)
+                {
+                    neuralNet.setInputs(0, dataset[i, 0]);
+                    neuralNet.setInputs(1, dataset[i, 1]);
+                    neuralNet.setInputs(2, dataset[i, 2]);
+                    neuralNet.setInputs(3, dataset[i, 3]);
+                    neuralNet.setDesiredOutput(0, dataset[i, 4]);
+                    neuralNet.learn();
+                }
+                
+                textBox10.Text = (++currentEpoch).ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label8.Text = "";
+
+            try
+            {
+                neuralNet.setInputs(0, Convert.ToDouble(textBox1.Text));
+                neuralNet.setInputs(1, Convert.ToDouble(textBox2.Text));
+                neuralNet.setInputs(2, Convert.ToDouble(textBox3.Text));
+                neuralNet.setInputs(3, Convert.ToDouble(textBox4.Text));
+                neuralNet.run();
+
+                double output = neuralNet.getOuputData(0);
+                textBox5.Text = output.ToString();
+                textBox6.Text = Convert.ToInt32(Math.Round(output)).ToString();
+            }
+            catch (Exception ex)
+            {
+                label8.Text = "Incorrect Inputs!";
+            }
+        }
+    }
+}
