54b529411a808c6922ca3f94d01585b22f3356b4
guadaaaaa <gnmlobando@gmail.com>
2024-12-15 12:46:37 +0800

    bug on add to cart buttons

--- Views/js/product_detail.js
+++ Views/js/product_detail.js
@@ -1,123 +1,120 @@
 $(document).ready(function () {
-    let user = sessionStorage.getItem('userDetails');
-    if (!user)
-        window.location.href = 'login.html';
+  let user = sessionStorage.getItem("userDetails");
+  if (!user) window.location.href = "login.html";
 
-    user = JSON.parse(user);
-    let product = sessionStorage.getItem('product');
+  user = JSON.parse(user);
+  let product = sessionStorage.getItem("product");
 
-    if (!product)
-        window.location.href = 'home.html';
+  if (!product) window.location.href = "home.html";
 
-    product = JSON.parse(product);
+  product = JSON.parse(product);
 
-    let cart = sessionStorage.getItem('cart');
-    cart = cart ? JSON.parse(cart) : [];
+  let cart = sessionStorage.getItem("cart");
+  cart = cart ? JSON.parse(cart) : [];
 
-    $('#name').html(product.name);
-    $('#description').html(product.description);
-    $('#price').html(product.price);
-    $('#stock').html(product.stock);
-    $('#productPictureUrl').attr('src', product.productPictureUrl);
+  $("#name").html(product.name);
+  $("#description").html(product.description);
+  $("#price").html(product.price);
+  $("#stock").html(product.stock);
+  $("#productPictureUrl").attr("src", product.productPictureUrl);
 
-    if (user.userID === product.userID) {
-        $('#owner').append(
-            `<button id="editButton" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-blue-200">Edit</button>
-            <button id="deleteButton" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded text-red-200">Delete</button>`
-        );
-    }
+  if (user.userID === product.userID) {
+    $("#owner").append(
+      `<button id="editButton" class="bg-[#436850] text-white hover:bg-[#365c45] focus:ring-4 focus:outline-none focus:ring-[#365c45] font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-white dark:text-[#436850] dark:hover:bg-[#436850] dark:hover:text-white dark:focus:ring-[#365c45]">Edit</button>
+            <button id="deleteButton" class="bg-[#436850] hover:bg-[#436850] text-white font-bold py-2 px-4 rounded text-white">Delete</button>`
+    );
+  }
 
-    fetch('http://localhost:5088/api/Ratings/product/' + product.productID)
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(function (rating) {
-                $('#comments').prepend(
-                    `<div class="bg-gray-200 border border-gray-300 p-2 rounded-lg mb-2">
+  fetch("http://localhost:5088/api/Ratings/product/" + product.productID)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach(function (rating) {
+        $("#comments").prepend(
+          `<div class="bg-gray-200 border border-gray-300 p-2 rounded-lg mb-2">
                         <p class="text-lg font-bold">${rating.username}</p>
                         <p>${rating.content == null ? "" : rating.content}</p>
                         <p>Rating: ${rating.stars}</p>` +
-                    (rating.imageUrl == null ? "" : `<img src="${rating.imageUrl}" alt="Product Image" class="w-full h-auto rounded-lg mt-2">`) +
-                    `</div>`
-                );
-            })
-        })
-        .catch(error => {
-            console.log(error);
-        });
-
-    $('#ratingForm').on('submit', function (e) {
-        e.preventDefault();
-
-        var formData = new FormData(this);
-        formData.append('userID', user.userID);
-        formData.append('productID', product.productID);
-
-        $.ajax({
-            url: 'http://localhost:5088/api/Ratings',
-            type: 'POST',
-            contentType: false,
-            processData: false,
-            data: formData,
-            success: function (response) {
-                alert('Rating added!');
-                window.location.reload();
-            },
-            error: function (xhr) {
-                if (xhr.status === 409) {
-                    alert("You have already created a rating!")
-                } else {
-                    alert('Error adding rating!')
-                }
-            }
-        });
+            (rating.imageUrl == null
+              ? ""
+              : `<img src="${rating.imageUrl}" alt="Product Image" class="w-full h-auto rounded-lg mt-2">`) +
+            `</div>`
+        );
+      });
+    })
+    .catch((error) => {
+      console.log(error);
     });
 
-    let currentQuantity = 1;
+  $("#ratingForm").on("submit", function (e) {
+    e.preventDefault();
 
-    $('#add-quantity').on('click', function () {
-        if (product.stock > currentQuantity)
-            currentQuantity++;
-        $('#quantity-count').html(currentQuantity);
-    });
+    var formData = new FormData(this);
+    formData.append("userID", user.userID);
+    formData.append("productID", product.productID);
 
-    $('#dec-quantity').on('click', function () {
-        if (currentQuantity > 1)
-            currentQuantity--;
-        $('#quantity-count').html(currentQuantity);
-    });
-
-    $('#addToCartButton').on('click', function () {
-        let productInCart = cart.find(p => p.productID === product.productID);
-
-        if (productInCart) {
-
-            if (productInCart.quantity + currentQuantity > product.stock) {
-                alert('Not enough stock!');
-                return;
-            }
-            productInCart.quantity += currentQuantity;
-        } else {
-            cart.push({
-                productID: product.productID,
-                quantity: currentQuantity
-            });
-        }
-
-        sessionStorage.setItem('cart', JSON.stringify(cart));
-
-        sessionStorage.removeItem('product');
-
-        alert('Product added to cart');
-
-        currentQuantity = 1;
-        $('#quantity-count').html(currentQuantity);
-
+    $.ajax({
+      url: "http://localhost:5088/api/Ratings",
+      type: "POST",
+      contentType: false,
+      processData: false,
+      data: formData,
+      success: function (response) {
+        alert("Rating added!");
         window.location.reload();
+      },
+      error: function (xhr) {
+        if (xhr.status === 409) {
+          alert("You have already created a rating!");
+        } else {
+          alert("Error adding rating!");
+        }
+      },
     });
+  });
 
-    $('#button-operation').prepend(
-        product.userID !== user.userID ?
-            `<div class="flex items-center mt-6"> 
+  let currentQuantity = 1;
+
+  $("#add-quantity").on("click", function () {
+    if (product.stock > currentQuantity) currentQuantity++;
+    $("#quantity-count").html(currentQuantity);
+  });
+
+  $("#dec-quantity").on("click", function () {
+    if (currentQuantity > 1) currentQuantity--;
+    $("#quantity-count").html(currentQuantity);
+  });
+
+  $("#addToCartButton").on("click", function () {
+    let productInCart = cart.find((p) => p.productID === product.productID);
+
+    if (productInCart) {
+      if (productInCart.quantity + currentQuantity > product.stock) {
+        alert("Not enough stock!");
+        return;
+      }
+      productInCart.quantity += currentQuantity;
+    } else {
+      cart.push({
+        productID: product.productID,
+        quantity: currentQuantity,
+      });
+    }
+
+    sessionStorage.setItem("cart", JSON.stringify(cart));
+
+    sessionStorage.removeItem("product");
+
+    alert("Product added to cart");
+
+    currentQuantity = 1;
+    $("#quantity-count").html(currentQuantity);
+
+    window.location.reload();
+  });
+
+  $("#button-operation").prepend(
+    product.userID !== user.userID
+      ? `<div class="flex items-center mt-6"> 
             <span class="text-lg font-medium mr-4">Quantity</span>
             <div class="flex items-center border rounded-lg px-2">
                 <button class="text-gray-700 text-lg font-bold" id="dec-quantity">-</button>
@@ -135,29 +132,27 @@ $(document).ready(function () {
             Add Rating
             </button>
             `
-        : 
-            `<button id="editButton" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-blue-200">Edit</button>
-            <button id="deleteButton" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded text-red-200">Delete</button>
+      : `<button id="editButton" class="bg-[#436850] hover:bg-green-500 text-white font-bold py-2 px-4 rounded text-white">Edit</button>
+            <button id="deleteButton" class="bg-red-700 text-white hover:bg-red-500 font-bold py-2 px-4 rounded">Delete</button>
             `
-    );
+  );
 
-    $('#editButton').on('click', function () {
-        sessionStorage.setItem('product', JSON.stringify(product));
-        window.location.href = 'edit_product.html';
+  $("#editButton").on("click", function () {
+    sessionStorage.setItem("product", JSON.stringify(product));
+    window.location.href = "edit_product.html";
+  });
+
+  $("#deleteButton").on("click", function () {
+    $.ajax({
+      url: `http://localhost:5088/api/Products/${product.productID}`,
+      type: "DELETE",
+      success: function () {
+        alert("Product deleted!");
+        window.location.href = "home.html";
+      },
+      error: function () {
+        alert("Error deleting product!");
+      },
     });
-
-    $('#deleteButton').on('click', function () {
-        $.ajax({
-            url: `http://localhost:5088/api/Products/${product.productID}`,
-            type: 'DELETE',
-            success: function () {
-                alert('Product deleted!');
-                window.location.href = 'home.html';
-            },
-            error: function () {
-                alert('Error deleting product!');
-            }
-        })
-    });
-
+  });
 });
