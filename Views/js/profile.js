35fe838f80768e58ff67e6cbc7fe5c8e0eeee6ad
Anato-Eini <jameskennethacabal@gmail.com>
2024-12-16 04:18:59 +0800

    Role in profile

--- Views/js/profile.js
+++ Views/js/profile.js
@@ -1,37 +1,48 @@
 $(document).ready(function () {
-  var user = sessionStorage.getItem("userDetails");
+    var user = sessionStorage.getItem("userDetails");
 
-  if (!user) window.location.href = "login.html";
+    if (!user) window.location.href = "login.html";
 
-  let cart = sessionStorage.getItem("cart");
-  cart = cart ? JSON.parse(cart) : [];
+    let cart = sessionStorage.getItem("cart");
+    cart = cart ? JSON.parse(cart) : [];
 
-  $('#cart-count').html(cart.length);
-  $('#goToCart').on('click', function () {
-    window.location.href = 'cart.html';
-  });
+    $('#cart-count').html(cart.length);
+    $('#goToCart').on('click', function () {
+        window.location.href = 'cart.html';
+    });
 
-  var userParsed = JSON.parse(user);
+    var userParsed = JSON.parse(user);
 
-  $("#username").html(userParsed.username);
-  $("#first-name").html(userParsed.firstName);
-  $("#last-name").html(userParsed.lastName);
-  $("#phone-number").html(userParsed.phoneNumber);
-  $("#address").html(userParsed.address);
-  $("#is-staff").html(userParsed.isStaff.toString());
-  $("#is-farmer").html(userParsed.isFarmer.toString());
-  $("#profile-picture").attr("src", userParsed.profilePictureUrl);
-  $('#profile-picture-desk').attr('src', userParsed.profilePictureUrl);
+    $("#username").html(userParsed.username);
+    $("#first-name").html(userParsed.firstName);
+    $("#last-name").html(userParsed.lastName);
+    $("#phone-number").html(userParsed.phoneNumber);
+    $("#address").html(userParsed.address);
+    $("#profile-picture").attr("src", userParsed.profilePictureUrl);
+    $('#profile-picture-desk').attr('src', userParsed.profilePictureUrl);
+    if (userParsed.isStaff == true && userParsed.isFarmer == true) {
+        $('#isStaff').html('Staff');
+        $('#isFarmer').html('Farmer');
+    } else {
+        $('#splitter').html('');
+        if (userParsed.isStaff == true) {
+            $('#isStaff').html('Staff');
+        } else if (userParsed.isFarmer == true) {
+            $('#isFarmer').html('Farmer');
+        }
+    }
 
-  const mobileMenuButton = document.getElementById("mobile-menu-button");
-  const mobileMenu = document.getElementById("mobile-menu");
-  const closeMobileMenuButton = document.getElementById("close-mobile-menu");
 
-  mobileMenuButton.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
-  });
 
-  closeMobileMenuButton.addEventListener("click", () => {
-    mobileMenu.classList.add("hidden");
-  });
+    const mobileMenuButton = document.getElementById("mobile-menu-button");
+    const mobileMenu = document.getElementById("mobile-menu");
+    const closeMobileMenuButton = document.getElementById("close-mobile-menu");
+
+    mobileMenuButton.addEventListener("click", () => {
+        mobileMenu.classList.toggle("hidden");
+    });
+
+    closeMobileMenuButton.addEventListener("click", () => {
+        mobileMenu.classList.add("hidden");
+    });
 });
